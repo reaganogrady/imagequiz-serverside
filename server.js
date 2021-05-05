172973decca6a24721fcc5aa26e5c6c10c640c7a
Reagan <reagan@email.arizona.edu>
2021-05-04 18:58:52 -0700

    Done

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let scores = [];
 app.use(express.json());
 
 app.get('/', (request, response) => {
-    response.send("Working");
+    response.send("imagequiz-serverside");
 });
 
 app.get('/quizzes', (request, response) => {
@@ -24,14 +24,14 @@ app.get('/quiz/:id', (request, response) => {
     if (quiz[id]){
         response.json(quiz[id]);
     } else {
-        response.status(404).send('Quiz id ${id} not found');
+        response.status(404).send('Quiz id not found');
     }
     
 });
 
 app.post('/score', (request, response) => {
-    let score = request.body.score;
-    scores.push(score);
+    let curr = {score: request.body.score, quizID: request.body.quizID, user: request.body.user};
+    scores.push(curr);
     response.send('Score added.');
 });
 
